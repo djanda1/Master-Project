3b2f8659c202038edb08f41be402582fabd589d6
djanda1 <115665881+djanda1@users.noreply.github.com>
2022-10-26 12:38:47 -0700

    Add files via upload

---
+++ writing.js
@@ -0,0 +1,42 @@
+let cnv;
+let d;
+let g;
+
+
+function setup() {
+  let x = 75;
+let y = 200;
+  createCanvas(1600,800);
+   background('rgb(206,206,248)');
+  line(x, y, (x + 12), (y - 25));
+  x = x + 18;
+  y = y - 38;
+ line(x, y, (x + 12), (y - 25));
+  x = x + 18;
+  y = y - 38;
+  line(x, y, (x + 12), (y - 25));
+  x = x + 18;
+  y = y - 38;
+  line(x, y, (x + 12), (y - 25));
+  x = x + 20;
+  y = y - 25;
+  line(x, y, (x + 12), (y + 25))
+  x = x + 18;
+  y = y + 38;
+  line(x, y, (x + 12), (y + 25))
+  x = x + 18;
+  y = y + 38;
+  line(x, y, (x + 12), (y + 25))
+  x = x + 18;
+  y = y + 38;
+  line(x, y, (x + 12), (y + 25))
+  line(115, 137, 140, 137)
+  line(150, 137, 175, 137)
+  
+}
+
+function draw() {
+  
+  
+  
+}
